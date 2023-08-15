287dd0a2b5fb17b1974776b4fab5451d68551504
haja <batchahaja@gmail.com>
2023-08-15 20:48:03 +0530

    customer

--- sgcf/Customer/models.py
+++ sgcf/Customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class mdlCustomer(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+   # agent = models.ForeignKey(Collectionagent, models.DO_NOTHING, db_column='Agent_ID')  # Field name made lowercase.
+    person = models.ForeignKey('Person', models.DO_NOTHING, db_column='Person_ID')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Customer'
